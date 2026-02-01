c6c0c9c51455c37212662764c0846bf4fb137926
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-01 14:44:14 +0100

    ..singular and plural

--- src/pages/PageTranslations.tsx
+++ src/pages/PageTranslations.tsx
@@ -1,6 +1,14 @@
 import { useTranslation } from "react-i18next";
 
 export const PageTranslations = () => {
-  const { t } = useTranslation();
-	return <p>{t("welcomeMessage")}</p>;
+	const { t } = useTranslation();
+	return (
+		<div>
+			<p>{t("welcomeMessage")}</p>
+			<p>{t("numberOfMessages", { count: 0 })}</p>
+			<p>{t("numberOfMessages", { count: 1 })}</p>
+			<p>{t("numberOfMessages", { count: 5 })}</p>
+			<p>{t("numberOfMessages", { count: 10 })}</p>
+		</div>
+	);
 };
--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -6,12 +6,16 @@ const resources = {
 		translation: {
 			title: "React Showcase",
 			welcomeMessage: "Welcome to React and react-i18next",
+			numberOfMessages_one: "You have {{count}} message.",
+			numberOfMessages_other: "You have {{count}} messages.",
 		},
 	},
 	fr: {
 		translation: {
 			title: "Démonstration React",
 			welcomeMessage: "Bienvenue à React et react-i18next",
+			numberOfMessages_one: "Vous avez {{count}} message.",
+			numberOfMessages_other: "Vous avez {{count}} messages.",
 		},
 	},
 };
@@ -20,7 +24,7 @@ i18n.use(initReactI18next) // passes i18n down to react-i18next
 	.init({
 		resources,
 		fallbackLng: "en",
-		lng: "en",
+		lng: "fr",
 		interpolation: {
 			escapeValue: false, // react already safes from xss
 		},
