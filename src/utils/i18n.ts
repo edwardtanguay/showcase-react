56983660ea66c1db2f4f385ae01781905d7f5db3
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-01 15:04:41 +0100

    ..comments out

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -22,13 +22,13 @@ const resources = {
 	},
 };
 
-i18n.use(initReactI18next) // passes i18n down to react-i18next
+i18n.use(initReactI18next)
 	.init({
 		resources,
 		fallbackLng: "en",
 		lng: "fr",
 		interpolation: {
-			escapeValue: false, // react already safes from xss
+			escapeValue: false,
 		},
 	});
 
