1882bdd4bd2815e6115794239e79872cd39ae607
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-01 12:58:49 +0100

    DEV: Change color of expand cards

--- src/components/TiltedBackgroundCard.tsx
+++ src/components/TiltedBackgroundCard.tsx
@@ -34,19 +34,19 @@ export const TiltedBackgroundCard = ({ children }: TiltedBackgroundCardProps) =>
 		<div className="flex justify-center p-6">
 			<div className="relative">
 				{/* tilted background card */}
-				<div className="max-w-[16rem] top-[.5rem] left-[.2rem] absolute inset-0 rounded-xl bg-green-900 -rotate-[5deg] scale-105 z-0"></div>
+				<div className="max-w-[16rem] top-[.5rem] left-[.2rem] absolute inset-0 rounded-xl bg-blue-900 -rotate-[5deg] scale-105 z-0"></div>
 
 				{/* main card: blurs see-through opacity */}
 				<div className="relative max-w-[16.5rem] rounded-xl border border-white backdrop-blur-sm text-gray-200 p-6 z-10">
 					{children}
 
 					{/* info icon */}
-					<div className="absolute top-[-18px] left-[40%] w-[30px] h-[30px] bg-green-900 rounded text-xs text-gray-300">
+					<div className="absolute top-[-18px] left-[40%] w-[30px] h-[30px] bg-blue-900 rounded text-xs text-gray-300">
 						<BsInfoSquareFill className="text-[2rem]" />
 					</div>
 
 					{/* word count */}
-					<p className="absolute bottom-[-14px] right-[-10px] w-fit h-fit p-1 border border-yellow-300 text-yellow-300 bg-green-900 rounded text-xs">
+					<p className="absolute bottom-[-14px] right-[-10px] w-fit h-fit p-1 border border-yellow-300 text-yellow-300 bg-blue-900 rounded text-xs">
 						{numberOfWords} word{numberOfWords === 1 ? '' : 's'}
 					</p>
 				</div>
