8cb91d8ba759b959bdab393e4bf08568fe6bc597
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-01 15:03:58 +0100

    ..tailwind works

--- src/pages/PageTranslations.tsx
+++ src/pages/PageTranslations.tsx
@@ -4,14 +4,19 @@ export const PageTranslations = () => {
 	const { t } = useTranslation();
 	return (
 		<div>
-      <h2 className="text-2xl mb-3">useTranslation</h2>
+			<h2 className="text-2xl mb-3">useTranslation</h2>
 			<p>{t("welcomeMessage")}</p>
 			<p>{t("numberOfMessages", { count: 0 })}</p>
 			<p>{t("numberOfMessages", { count: 1 })}</p>
 			<p>{t("numberOfMessages", { count: 5 })}</p>
 			<p>{t("numberOfMessages", { count: 10 })}</p>
-      <h2 className="text-2xl mt-3 mb-3">Trans</h2>
-      <p><Trans i18nKey="invitation" /></p>
+			<h2 className="text-2xl mt-3 mb-3">Trans</h2>
+			<p>
+				<Trans
+					i18nKey="invitation"
+					components={[<span className="text-red-700 font-bold" />]}
+				/>
+			</p>
 		</div>
 	);
 };
