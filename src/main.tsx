b2a6736a0532e5f0a6469e29a32709928121a916
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-01 15:22:56 +0100

    ..translations in json files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import "./index.scss";
-import "./utils/i18n.ts";
+import "./utils/i18n/index.ts";
 import { PageWelcome } from "./pages/PageWelcome.tsx";
 import { Page404 } from "./pages/Page404.tsx";
 import { AppProvider } from "./AppContext.tsx";
---
+++ src/utils/i18n/index.ts
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enLang from "./locales/en/en.json";
+import frLang from "./locales/fr/fr.json";
+
+const resources = {
+	en: {
+		translation: enLang,
+	},
+	fr: {
+		translation: frLang,
+	},
+};
+
+i18n.use(initReactI18next).init({
+	resources,
+	fallbackLng: "en",
+	lng: "fr",
+	interpolation: {
+		escapeValue: false,
+	},
+});
+
+export default i18n;
